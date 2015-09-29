619a8b18440ae2f932388f2925ea55c91951708e
王小龙 <1036199213@qq.com>
2015-09-29 14:45:03 +0800

    文本编辑

--- src/hetao/ui/ieditor/activity/EditTextActivity.java
+++ src/hetao/ui/ieditor/activity/EditTextActivity.java
@@ -3,23 +3,33 @@ package hetao.ui.ieditor.activity;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.WindowManager.LayoutParams;
+import android.widget.EditText;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 public class EditTextActivity  extends Activity{
     
-	TextView textView;
+	EditText textView;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 		RelativeLayout rl =new RelativeLayout(this);
 		
-		LayoutParams lp =(LayoutParams) rl.getLayoutParams();
+		LayoutParams lp =new LayoutParams();
+
 		lp.width = LayoutParams.MATCH_PARENT;
 		lp.height = LayoutParams.MATCH_PARENT;
 		
-		textView =new TextView(this);
+		LayoutParams lp2 =new LayoutParams();
+
+		lp2.width = LayoutParams.MATCH_PARENT;
+		lp2.height = LayoutParams.WRAP_CONTENT;
+		lp2.horizontalMargin=10;
+		
+		textView =new EditText(this);
+		textView.setLayoutParams(lp2);
+		textView.setText("哈哈哈");
 		
 		rl.addView(textView);
 		
